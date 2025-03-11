0595c4ada7d86dbf07b47d99e3b2743540327e22
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-03-11 12:32:33 +0000

    update de tickets hecho

--- src/views/Panell.jsx
+++ src/views/Panell.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
-import { dades_tiquets } from "../database/gestionTickets";
-import { getDadesTiquets, getDadesUsuaris } from "../database/gestionTickets";
+import { getDadesTiquets } from "../database/gestionTickets";
 import { useNavigate } from "react-router-dom";
 import Tiquet from "./tiquet";
+import TiquetsPendents from "../components/TiquetsPendents";
+import TiquetsResolts from "../components/TiquetsResolts";
 
 const Panell = () => {
   const storedTickets = getDadesTiquets();
@@ -15,6 +16,7 @@ const Panell = () => {
   const [tickets, setTickets] = useState(storedTickets);
   const [showModal, setShowModal] = useState(false);
   const [currentTicket, setCurrentTicket] = useState(null);
+
   const updateLocalStorage = (updatedTickets) => {
     localStorage.setItem("tickets", JSON.stringify(updatedTickets));
   };
@@ -54,13 +56,17 @@ const Panell = () => {
 
   const closeModal = () => {
     setShowModal(false);
+    setCurrentTicket(null); // Limpiar al cerrar el modal
   };
 
   const handleNavigateToComentarios = (ticketId) => {
     navigate(`/comentaris/${ticketId}`, {});
   };
 
-  console.log(tickets);
+  const editTicket = (ticket) => {
+    setCurrentTicket(ticket);
+    setShowModal(true); // Abrir modal de edición
+  };
 
   return (
     <>
@@ -80,193 +86,31 @@ const Panell = () => {
           show={showModal}
           handleClose={closeModal}
           onAddTicket={updateTickets}
+          currentTicket={currentTicket} // Enviar el ticket actual a editar
         />
 
-        <h2 className="mt-5">Tickets pendientes</h2>
-        <table className="table mt-4">
-          <thead>
-            <tr>
-              <th>Código</th>
-              <th>Fecha</th>
-              <th>Aula</th>
-              <th>Grupo</th>
-              <th>Ordenador</th>
-              <th>Descripción</th>
-              <th>Alumno</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tickets.map(
-              (ticket) =>
-                ticket.Estado === "Pendiente" && (
-                  <tr key={ticket.Codigo}>
-                    <td>{ticket.Codigo}</td>
-                    <td>{ticket.Fecha}</td>
-                    <td>{ticket.Aula}</td>
-                    <td>{ticket.Grupo}</td>
-                    <td>{ticket.Ordenador}</td>
-                    <td>{ticket.Descripcion}</td>
-                    <td>{ticket.Alumno}</td>
-                    {rol === "Admin" && (
-                      <td>
-                        <button
-                          className="btn btn-success"
-                          title="Resolver ticket"
-                          onClick={() => resolveTicket(ticket.Codigo)}
-                        >
-                          Resolver
-                        </button>
-                      </td>
-                    )}
+        <TiquetsPendents
+          tickets={tickets}
+          resolveTicket={resolveTicket}
+          deleteTicket={deleteTicket}
+          rol={rol}
+          handleNavigateToComentarios={handleNavigateToComentarios}
+          handleModal={handleModal}
+          setCurrentTicket={setCurrentTicket}
+          editTicket={editTicket}
+        />
 
-                    {rol === "Admin" && (
-                      <td>
-                        <button
-                          onClick={() => {
-                            handleModal();
-                            setCurrentTicket(ticket);
-                          }}
-                          className="btn btn-warning"
-                          title="Añadir comentario"
-                        >
-                          <i
-                            className="bi bi-pencil"
-                            data-bs-toggle="modal"
-                            data-bs-target="#exampleModal"
-                          ></i>
-                        </button>
-                      </td>
-                    )}
-                    <td>
-                      <button
-                        onClick={() => {
-                          handleNavigateToComentarios(ticket.Codigo);
-                          handleModal();
-                          setCurrentTicket(ticket);
-                        }}
-                        className="btn btn-info"
-                        title="Ver comentarios"
-                      >
-                        <i className="bi bi-chat-left-text"></i>
-                      </button>
-                    </td>
-                    {rol === "Admin" && (
-                      <td>
-                        <button
-                          className="btn btn-danger"
-                          title="Eliminar ticket"
-                          onClick={() => deleteTicket(ticket.Codigo)}
-                        >
-                          <i className="bi bi-trash3"></i>
-                        </button>
-                      </td>
-                    )}
-                  </tr>
-                )
-            )}
-          </tbody>
-        </table>
-
-        {rol === "Admin" && (
-          <>
-            <h2 className="mt-5">Tickets resueltos</h2>
-            <table className="table mt-4">
-              <thead>
-                <tr>
-                  <th>Código</th>
-                  <th>Fecha</th>
-                  <th>Fecha resuelto</th>
-                  <th>Aula</th>
-                  <th>Grupo</th>
-                  <th>Ordenador</th>
-                  <th>Descripción</th>
-                  <th>Alumno</th>
-                </tr>
-              </thead>
-              <tbody>
-                {tickets.map((ticket) =>
-                  ticket.Estado === "Resuelto" ? (
-                    <tr key={ticket.Codigo}>
-                      <td>{ticket.Codigo}</td>
-                      <td>{ticket.Fecha}</td>
-                      <td>{ticket.fechaResuelto}</td>
-                      <td>{ticket.Aula}</td>
-                      <td>{ticket.Grupo}</td>
-                      <td>{ticket.Ordenador}</td>
-                      <td>{ticket.Descripcion}</td>
-                      <td>{ticket.Alumno}</td>
-                      <td>
-                        <button
-                          className="btn btn-success"
-                          title="Resolver ticket"
-                        >
-                          Resolver
-                        </button>
-                      </td>
-                      <td>
-                        <button
-                          onClick={handleModal}
-                          className="btn btn-warning"
-                          title="Añadir comentario"
-                        >
-                          <i
-                            className="bi bi-pencil"
-                            data-bs-toggle="modal"
-                            data-bs-target="#exampleModal"
-                          ></i>
-                        </button>
-                      </td>
-                      <td>
-                        <button
-                          onClick={() => {
-                            handleNavigateToComentarios(ticket.Codigo);
-                            handleModal();
-                            setCurrentTicket(ticket);
-                          }}
-                          className="btn btn-info"
-                          title="Ver comentarios"
-                        >
-                          <i className="bi bi-chat-left-text"></i>
-                        </button>
-                      </td>
-                      <td>
-                        <button
-                          className="btn btn-danger"
-                          title="Eliminar ticket"
-                          onClick={() => deleteTicket(ticket.Codigo)}
-                        >
-                          <i className="bi bi-trash3"></i>
-                        </button>
-                      </td>
-                    </tr>
-                  ) : null
-                )}
-              </tbody>
-            </table>
-          </>
-        )}
+        <TiquetsResolts
+          tickets={tickets}
+          resolveTicket={resolveTicket}
+          deleteTicket={deleteTicket}
+          rol={rol}
+          handleNavigateToComentarios={handleNavigateToComentarios}
+          handleModal={handleModal}
+          setCurrentTicket={setCurrentTicket}
+          editTicket={editTicket}
+        />
       </main>
-
-      {/* <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Observaciones</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              <p>Código incidencia: <span>123546</span></p>
-              <label htmlFor="comentario" className="form-label">Comentario:</label>
-              <input className="form-control" defaultValue="Este es un comentario sobre esta incidencia" />
-              <p className="small text-end">Autor: <span>Pepe Loco</span></p>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-              <button type="button" className="btn btn-primary">Guardar cambios</button>
-            </div>
-          </div>
-        </div>
-      </div> */}
 
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
